10ef7024a06aa4f3ef88aee6efcceac77b9a92e6
kaelzhang81 <zhangy1981@qq.com>
2015-04-25 09:50:30 +0800

    create
    add files

---
+++ robotide/RIDE.bat
@@ -0,0 +1,8 @@
+@if exist "C:\Documents and Settings\Administrator\Application Data\RobotFramework" rd /s /q "C:\Documents and Settings\Administrator\Application Data\RobotFramework" 
+
+c:
+
+cd C:\Python27\Lib\site-packages\robotide
+
+python __init__.py
+
